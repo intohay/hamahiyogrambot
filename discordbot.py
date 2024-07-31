efca09a3f46d44bacd7e9c32143a908d57a691af
root2116 <root6889@icloud.com>
2024-07-31 09:53:29 +0900

    check stories every 30 minutes

--- discordbot.py
+++ discordbot.py
@@ -18,17 +18,19 @@ profile = instaloader.Profile.from_username(L.context, username)
 
 
 last_check_file = f"{username}_last_check.txt"
+last_story_check_file = f"{username}_last_story_check.txt"
 
-def load_last_check_time():
-    if os.path.exists(last_check_file):
-        with open(last_check_file, 'r') as file:
-            timestamp = file.read().strip()
+def load_last_check_time(file):
+    if os.path.exists(file):
+        with open(file, 'r') as f:
+            timestamp = f.read().strip()
             return datetime.strptime(timestamp, '%Y-%m-%d_%H-%M-%S')
     return None
 
-def save_last_check_time(timestamp):
-    with open(last_check_file, 'w') as file:
-        file.write(timestamp.strftime('%Y-%m-%d_%H-%M-%S'))
+def save_last_check_time(timestamp, file):
+    with open(file, 'w') as f:
+        f.write(timestamp.strftime('%Y-%m-%d_%H-%M-%S'))
+
 
 # Discord Botのセットアップ
 intents = discord.Intents.all()
@@ -40,20 +42,17 @@ async def download_and_post():
     channel = client.get_channel(config.CHANNEL_ID)
 
     while not client.is_closed():
-        last_check_time = load_last_check_time()
+        last_check_time = load_last_check_time(last_check_file)
         new_last_check_time = last_check_time
 
         post_dir = f"{username}_posts"
-        story_dir = f"{username}_stories"
 
         print("Downloading posts...")
         posts = []
-        stories = []    
 
         for post in profile.get_posts():
             post_time = post.date_utc
             if last_check_time is None or post_time > last_check_time:
-                
                 if not os.path.exists(post_dir):
                     os.makedirs(post_dir)
                 L.download_post(post, target=post_dir)
@@ -67,44 +66,49 @@ async def download_and_post():
         for post in posts:
             await post_all_media_to_discord(channel, post_dir, post)
         
-        # print("Downloading reels...")
-        # for reel in profile.get_igtv_posts():
-        #     reel_time = reel.date_utc
-        #     if last_check_time is None or reel_time > last_check_time:
-        #         reel_dir = f"{username}_reels"
-        #         if not os.path.exists(reel_dir):
-        #             os.makedirs(reel_dir)
-        #         L.download_post(reel, target=reel_dir)
-        #         await post_all_media_to_discord(channel, reel_dir, reel)
-        #         if new_last_check_time is None or reel_time > new_last_check_time:
-        #             new_last_check_time = reel_time
-        
+        if new_last_check_time:
+            save_last_check_time(new_last_check_time, last_check_file)
+
+        print("All content has been downloaded.")
+
+        await asyncio.sleep(60)  # 1分ごとに実行
+
+async def download_and_post_stories():
+    await client.wait_until_ready()
+    channel = client.get_channel(config.CHANNEL_ID)
+
+    while not client.is_closed():
+        last_story_check_time = load_last_check_time(last_story_check_file)
+        new_last_story_check_time = last_story_check_time
+
+        story_dir = f"{username}_stories"
+
         print("Downloading stories...")
+        stories = []
+
         for story in L.get_stories(userids=[profile.userid]):
             for item in story.get_items():
                 item_time = item.date_utc
-                if last_check_time is None or item_time > last_check_time:
-                    
+                if last_story_check_time is None or item_time > last_story_check_time:
                     if not os.path.exists(story_dir):
                         os.makedirs(story_dir)
                     L.download_storyitem(item, target=story_dir)
                     stories.append(item)
-                    if new_last_check_time is None or item_time > new_last_check_time:
-                        new_last_check_time = item_time
+                    if new_last_story_check_time is None or item_time > new_last_story_check_time:
+                        new_last_story_check_time = item_time
                 else:
                     break
         
         stories.reverse()
-
         for story in stories:
             await post_all_media_to_discord(channel, story_dir, story)
 
-        if new_last_check_time:
-            save_last_check_time(new_last_check_time)
+        if new_last_story_check_time:
+            save_last_check_time(new_last_story_check_time, last_story_check_file)
 
-        print("All content has been downloaded.")
+        print("All stories have been downloaded.")
 
-        await asyncio.sleep(60) # 1分ごとに実行
+        await asyncio.sleep(1800)  # 30分ごとに実行
 
 async def post_all_media_to_discord(channel, post_dir, post):
     post_timestamp = post.date_utc.strftime('%Y-%m-%d_%H-%M-%S')
